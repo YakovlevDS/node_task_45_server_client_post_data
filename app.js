37d16a0b792e159724bb0a6a50df37facdd7836c
Dmitriy Yakovlev <1d3ddd@gmail.com>
2022-05-17 22:06:33 +0300

    feat:add get data from client

--- app.js
+++ app.js
@@ -1,12 +1,45 @@
-let nodePath = process.argv[0];
-let appPath = process.argv[1];
-let name = process.argv[2];
-let age = process.argv[3];
 
-console.log("nodePath: " + nodePath);
-console.log("appPath: " + appPath);
-console.log();
-console.log("name: " + name);
-console.log("age: " + age);
+// const http = require("http");
+// const fs = require("fs");
+ 
+// http.createServer((req, res) => {
+      
+//      if (req.url === "/user") {
+         
+//         let data = "";
+//         req.on("data", chunk => {
+//             data += chunk;
+//         });
+//         req.on("end", () => {
+//             console.log(data);
+//             res.end("Данные успешно получены");
+//         });
+//     }
+//     else{
+//         fs.readFile("index.html", (e, data) => res.end(data));
+//     }
+// }).listen(3000, ()=>console.log("Сервер запущен по адресу http://localhost:3000"));
 
-// node app.js Tom 23
+// solution 2
+
+const http = require("http");
+const fs = require("fs");
+ 
+http.createServer(async (request, response) => {
+      
+     if (request.url === "/user") {
+         
+        const buffers = []; // буфер для получаемых данных
+ 
+        for await (const chunk of request) {
+            buffers.push(chunk);        // добавляем в буфер все полученные данные
+        }
+ 
+        const data = Buffer.concat(buffers).toString();
+        console.log(data);
+        response.end("Данные успешно получены");
+    }
+    else{
+        fs.readFile("index.html", (error, data) => response.end(data));
+    }
+}).listen(3000, ()=>console.log("Сервер запущен по адресу http://localhost:3000"));
