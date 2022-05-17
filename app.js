b9a60886798f93e92f8152dbf6e7b099c454f654
Dmitriy Yakovlev <1d3ddd@gmail.com>
2022-05-17 22:12:57 +0300

    feat: add json parse

--- app.js
+++ app.js
@@ -22,6 +22,30 @@
 
 // solution 2
 
+// const http = require("http");
+// const fs = require("fs");
+ 
+// http.createServer(async (request, response) => {
+      
+//      if (request.url === "/user") {
+         
+//         const buffers = []; // буфер для получаемых данных
+ 
+//         for await (const chunk of request) {
+//             buffers.push(chunk);        // добавляем в буфер все полученные данные
+//         }
+ 
+//         const data = Buffer.concat(buffers).toString();
+//         console.log(data);
+//         response.end("Данные успешно получены");
+//     }
+//     else{
+//         fs.readFile("index.html", (error, data) => response.end(data));
+//     }
+// }).listen(3000, ()=>console.log("Сервер запущен по адресу http://localhost:3000"));
+
+// solution 3
+
 const http = require("http");
 const fs = require("fs");
  
@@ -35,8 +59,8 @@ http.createServer(async (request, response) => {
             buffers.push(chunk);        // добавляем в буфер все полученные данные
         }
  
-        const data = Buffer.concat(buffers).toString();
-        console.log(data);
+        const user = JSON.parse(Buffer.concat(buffers).toString());
+        console.log(user.name,"-", user.age);
         response.end("Данные успешно получены");
     }
     else{
